c77d66dcb437315ae844dccb9b86b19480bbafcc
JPig-Main <jumpei141107@gmail.com>
2021-06-22 11:16:07 +0900

    ADD:ボタンによるゲーム進行

--- app.js
+++ app.js
@@ -56,7 +56,6 @@ const xyWayForEach = func => {
     })
 }
 
-// TODO すごろくの進む順を定義する
 class Order {
     #values = [
         new XY(0, 0),
@@ -75,7 +74,7 @@ class Order {
         'goal': this.#values.length - 1,
         'previous': undefined,
     };
-    
+
     #findValue = (xy) => {
         for (let i = 0; i < this.#values.length; i++) {
             if (xy.equals(this.#values[i])) {
@@ -119,14 +118,23 @@ class Order {
         // 前の位置を記憶
         this.#specials.previous = this.#now;
 
+        const result = {
+            'advanceN': undefined,
+            'doesGoal': undefined,
+        }
+
         // 駒を進める
         if (this.#now + n >= this.#values.length - 1) {
+            result.advanceN = this.#specials.goal - this.#now;
             this.#now = this.#getSpecial('goal');
+            result.doesGoal = true;
         }
         else {
             this.#now += n;
+            result.advanceN = n;
+            result.doesGoal = false
         }
-        return true;
+        return result;
     }
     reset = () => {
         this.#specials.previous = undefined;
@@ -135,16 +143,19 @@ class Order {
 }
 
 class Board {
+    #order = new Order();
+    #size = new XY(4, 3);
+    #range;
+    #values;
     constructor() {
-        this.order = new Order();
-        this.size = new XY(4, 3)
-        this.range = new XYRange(0, this.size.x - 1, 0, this.size.y - 1);
-        this.values = this.order.toBoard(this.range.value);
-        this.initRender();
+        this.#range = new XYRange(0, this.#size.x - 1, 0, this.#size.y - 1);
+        this.#values = this.#order.toBoard(this.#range.value);
+        this.#initRender();
+        this.#setStart();
     }
-    getValue = xy => this.values[xy.y][xy.x];
+    getValue = xy => this.#values[xy.y][xy.x];
     valuesForEach = (rowsFunc, valueFunc) => {
-        this.values.forEach((rows, y) => {
+        this.#values.forEach((rows, y) => {
             rowsFunc(y);
             rows.forEach((value, x) => {
                 valueFunc(new XY(x, y));
@@ -154,41 +165,47 @@ class Board {
     getThDOM = xy => $(`tr#rows-${xy.y} > th#value-${xy.x}`);
     #getClassName = xy => this.getValue(xy) ? 'no-empty' : 'empty';
 
-    initRender = () => {
+    #initRender = () => {
         this.valuesForEach((y) => {
             $('tbody').append(`<tr id="rows-${y}"></tr>`);
         }, (xy) => {
             $(`#rows-${xy.y}`).append(`<th id="value-${xy.x}" class="${this.#getClassName(xy)}"></th>`);
         })
     }
-    reRender = () => {
-        if (!this.order.getIsOn('start')) {
-            this.getThDOM(this.order.getXy('previous')).removeClass('now');
+    #reRender = () => {
+        if (!this.#order.getIsOn('start')) {
+            this.getThDOM(this.#order.getXy('previous')).removeClass('now');
         }
-        this.getThDOM(this.order.getXy('now')).addClass('now');
+        this.getThDOM(this.#order.getXy('now')).addClass('now');
     }
 
-    setStart = () => {
-        this.order.setStart();
-        this.reRender();
+    #setStart = () => {
+        this.#order.setStart();
+        this.#reRender();
     }
-    advance = (n) => {
+    #advance = (n) => {
         // マスを進める
-        if (!this.order.advance(n)) {
+        const result = this.#order.advance(n);
+        if (!result) {
             return false;
         }
         // 再描画して反映
-        this.reRender();
-        return true;
+        this.#reRender();
+        return result;
+    }
+    randomAdvance = () => {
+        const result = this.#advance(Math.floor(Math.random() * 6) + 1);
+        console.log(result ? `${result.advanceN}マス進みました` : 'すでにゴールしています');
+        if (result && result.doesGoal) {
+            console.log('ゴールしました');
+        }
     }
     reset = () => {
-        this.getThDOM(this.order.getXy('now')).removeClass('now');
-        this.order.reset();
-        this.setStart();
-        this.reRender();
+        this.getThDOM(this.#order.getXy('now')).removeClass('now');
+        this.#order.reset();
+        this.#setStart();
+        console.log('リセットしました');
     }
 }
 
 const board = new Board();
-
-board.setStart();
